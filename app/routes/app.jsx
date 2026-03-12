91edc1bb1eb13d97bcbebaddf484dd9166e02932
Balvant <balvant@m2webdesigning.com>
2026-03-12 12:44:36 +0530

    Fixed Issue App

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -85,7 +85,7 @@ export default function App() {
           <s-link href={host ? `/app/rules?host=${encodeURIComponent(host)}` : "/app/rules"}>Cart Rule</s-link>
           {/* <s-link href={host ? `/app/pricing?host=${encodeURIComponent(host)}` : "/app/pricing"}>Pricing</s-link> */}
           <s-link href="https://cartliftcartdrawerupsell.tawk.help/category/features" target="_blank" rel="noopener noreferrer">Documents</s-link>
-          <s-link href={host ? `/app/help?host=${encodeURIComponent(host)}` : "/app/help"}>Help</s-link>
+          {/* <s-link href={host ? `/app/help?host=${encodeURIComponent(host)}` : "/app/help"}>Help</s-link> */}
         </s-app-nav>
 
         {/* Nested routes render here */}
