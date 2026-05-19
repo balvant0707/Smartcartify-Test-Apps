7e07fc42b56f8c0f6d7dde3ca161333b50f095a6
balvant0707 <baldevsachiya@gmail.com>
2026-05-19 17:55:11 +0530

    19-05-2026

---
+++ app/routes/sitemap[.xml].jsx
@@ -0,0 +1,6 @@
+export const loader = () => {
+  return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>`,
+    { status: 200, headers: { "Content-Type": "application/xml" } }
+  );
+};
