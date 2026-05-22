ccc85e8dde0ce59743fa2d81b2d304de0769f375
balvant0707 <baldevsachiya@gmail.com>
2026-05-22 14:24:19 +0530

    21-05-2026

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -208,6 +208,7 @@ export default function App() {
         <s-app-nav>
           {/* <s-link href={host ? `/app?host=${encodeURIComponent(host)}` : "/app"}>Dashboard</s-link> */}
           <s-link href={host ? `/app/rules?host=${encodeURIComponent(host)}` : "/app/rules"}>Cart Rule</s-link>
+          <s-link href={host ? `/app/customize-preview?host=${encodeURIComponent(host)}` : "/app/customize-preview"}>Customize Preview</s-link>
           <s-link href={host ? `/app/campaigns?host=${encodeURIComponent(host)}` : "/app/campaigns"}>Create Campaign</s-link>
           <s-link href={host ? `/app/my-rules?host=${encodeURIComponent(host)}` : "/app/my-rules"}>My Rules</s-link>
           <s-link href={host ? `/app/cartbar?host=${encodeURIComponent(host)}` : "/app/cartbar"}>Add to Cart Bar</s-link>
